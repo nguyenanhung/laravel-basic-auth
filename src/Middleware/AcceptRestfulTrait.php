d6f2452936ce3a2485722f2503a66cbae9cdaf8b
nguyenanhung <hungna.dev@gmail.com>
2024-10-30 23:23:00 +0700

    Fix AcceptRestfulTrait header incorrect

--- src/Middleware/AcceptRestfulTrait.php
+++ src/Middleware/AcceptRestfulTrait.php
@@ -10,8 +10,8 @@ trait AcceptRestfulTrait
     protected function acceptRequestRestful($origin, Request $request, Closure $next)
     {
         return $next($request)
-            ->header('Access - Control - Allow - Origin', $origin)
-            ->header('Access - Control - Allow - Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access - Control - Allow - Headers', 'Content - Type, Authorization');
+            ->header('Access-Control-Allow-Origin', $origin)
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
 }
